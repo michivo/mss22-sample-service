6cb49ea6fab45b2a372195826b33d7e06141cabf
Michael Faschinger <michivo@gmail.com>
2022-02-26 23:17:19 +0100

    return dto when adding new item

--- server/controllers/articles.controller.js
+++ server/controllers/articles.controller.js
@@ -4,8 +4,8 @@ const articleDao = require('../models/DAOs/articles.model');
 const articleDto = require('../models/DTOs/article');
 
 const add = async (req, res, _) => {
-    await articleDao.add(req.body);
-    res.status(200).send('OK').end();
+    const addedItem = await articleDao.add(req.body);
+    res.status(200).send(articleDto(addedItem)).end();
 };
 
 const get = async (_, res, __) => {
